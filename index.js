65e8c73cf279002c51d9a5e5dd834d81144b2820
Oliver Joseph Ash <oliverjash@icloud.com>
2014-02-19 17:22:44 +0000

    Only start server once

--- index.js
+++ index.js
@@ -4,28 +4,29 @@ var tinylr = require('tiny-lr');
 module.exports = function() {
     // TODO: lazy initialise when needed only
     var port = 35729;
-    var server = tinylr({port: port});
-    server.server.removeAllListeners('error');
-    server.server.on('error', function(err) {
-        if (err.code === 'EADDRINUSE') {
-            console.error('Port ' + port + ' is already in use by another process.');
-        } else {
-            console.error(err);
-        }
-        process.exit(1);
-    });
-    server.listen(port, function(err) {
-        if (err) { return console.error(err); }
-        console.log('Live reload server started on port: ' + port);
-    });
+    var server;
 
     return function(reports) {
+        if (! server) {
+            server = tinylr({port: port});
+            server.server.removeAllListeners('error');
+            server.server.on('error', function(err) {
+                if (err.code === 'EADDRINUSE') {
+                    console.error('Port ' + port + ' is already in use by another process.');
+                } else {
+                    console.error(err);
+                }
+                process.exit(1);
+            });
+            server.listen(port, function(err) {
+                if (err) { return console.error(err); }
+                console.log('Live reload server started on port: ' + port);
+            });
+        }
+
         // FIXME: filter reports only
         if (reports.length > 1) {
             var files = reports.filter(function (report) {
-                // Filter out source maps — LiveReload will refresh the whole
-                // page when it detects a non-CSS file.
-                // FIXME: Add API for detecting source map reports.
                 return ! report.path.filename().match(/\.map$/);
             }).map(function(report) {
                 return report.path.absolute();
